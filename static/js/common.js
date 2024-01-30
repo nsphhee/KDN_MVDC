af6e32f92d162189b084f8af6ffed4467be762e1
nsphhee <parkhhee@ninesoft.biz>
2024-01-30 14:01:18 +0900

    레이어팝업

--- static/js/common.js
+++ static/js/common.js
@@ -1,31 +1,4 @@
 $(document).ready(function() {    
-    // 메인 탭
-    const tabList = document.querySelectorAll('.tab-default .t-list li');
-    const contents = document.querySelectorAll('.tab-box .tab-cont')
-    let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
-
-    for(var i = 0; i < tabList.length; i++){
-        if ( tabList[i].querySelector('.t-tit') != null ) {
-            tabList[i].querySelector('.t-tit').addEventListener('click', function(e){
-                e.preventDefault();
-                for(var j = 0; j < tabList.length; j++){
-                    // 나머지 버튼 클래스 제거
-                    tabList[j].classList.remove('t-on');
-
-                    // 나머지 컨텐츠 display:none 처리
-                    contents[j].style.display = 'none';   
-                }
-
-                // 버튼 관련 이벤트
-                this.parentNode.classList.add('t-on');
-
-                // 버튼 클릭시 컨텐츠 전환
-                activeCont = this.getAttribute('href');
-                document.querySelector(activeCont).style.display = 'block';
-            });
-        }
-    }
-
 
     // 레이어팝업
     // jQuery 환경에서만 작동할 수 있게 처리
@@ -101,6 +74,34 @@ $(document).ready(function() {
             }
         });
     });
+
+    
+    // 메인 탭
+    const tabList = document.querySelectorAll('.tab-default .t-list li');
+    const contents = document.querySelectorAll('.tab-box .tab-cont')
+    let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+
+    for(var i = 0; i < tabList.length; i++){
+        if ( tabList[i].querySelector('.t-tit') != null ) {
+            tabList[i].querySelector('.t-tit').addEventListener('click', function(e){
+                e.preventDefault();
+                for(var j = 0; j < tabList.length; j++){
+                    // 나머지 버튼 클래스 제거
+                    tabList[j].classList.remove('t-on');
+
+                    // 나머지 컨텐츠 display:none 처리
+                    contents[j].style.display = 'none';   
+                }
+
+                // 버튼 관련 이벤트
+                this.parentNode.classList.add('t-on');
+
+                // 버튼 클릭시 컨텐츠 전환
+                activeCont = this.getAttribute('href');
+                document.querySelector(activeCont).style.display = 'block';
+            });
+        }
+    }
 
 
 });
