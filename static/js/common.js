2117d5953073e1b9a51b690543d447003e9a24d6
nsphhee <parkhhee@ninesoft.biz>
2024-01-31 13:58:00 +0900

    .

--- static/js/common.js
+++ static/js/common.js
@@ -76,9 +76,9 @@ $(document).ready(function() {
     });
 
     
-    // 메인 탭
-    const tabList = document.querySelectorAll('.tab-default .t-list li');
-    const contents = document.querySelectorAll('.tab-box .tab-cont')
+    // 탭 - 레이어팝업
+    const tabList = document.querySelectorAll('.tab-default .tab-h-list li');
+    const contents = document.querySelectorAll('.tab-cont .tab-c-item')
     let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
 
     for(var i = 0; i < tabList.length; i++){
@@ -87,14 +87,14 @@ $(document).ready(function() {
                 e.preventDefault();
                 for(var j = 0; j < tabList.length; j++){
                     // 나머지 버튼 클래스 제거
-                    tabList[j].classList.remove('t-on');
+                    tabList[j].classList.remove('tab-h-on');
 
                     // 나머지 컨텐츠 display:none 처리
                     contents[j].style.display = 'none';   
                 }
 
                 // 버튼 관련 이벤트
-                this.parentNode.classList.add('t-on');
+                this.parentNode.classList.add('tab-h-on');
 
                 // 버튼 클릭시 컨텐츠 전환
                 activeCont = this.getAttribute('href');
