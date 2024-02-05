c5445072fd4406ce021ea548f7a8de861f65862c
nsphhee <parkhhee@ninesoft.biz>
2024-02-05 09:59:01 +0900

    .

--- static/js/common.js
+++ static/js/common.js
@@ -105,6 +105,10 @@ $(document).ready(function() {
     const cmTabList = document.querySelectorAll('.cm-tab-default .tab-h-list li'); //홈,설정 탭
     const cmContents = document.querySelectorAll('.cm-tab-cont .tab-c-item'); //홈,설정 탭
 
+    const cMidTabList = document.querySelectorAll('.c-mid-tab-default .tab-h-list li'); //홈,설정 탭
+    const cMidContents = document.querySelectorAll('.c-mid-tab-cont .tab-c-item'); //홈,설정 탭
+
+
     let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
 
     for(var i = 0; i < lpTabList.length; i++){
@@ -151,6 +155,28 @@ $(document).ready(function() {
         }
     };
     
+
+    for(var i = 0; i < cMidTabList.length; i++){
+        if ( cMidTabList[i].querySelector('.t-tit') != null ) {
+            cMidTabList[i].querySelector('.t-tit').addEventListener('click', function(e){
+                e.preventDefault();
+                for(var j = 0; j < cMidTabList.length; j++){
+                    // 나머지 버튼 클래스 제거
+                    cMidTabList[j].classList.remove('tab-h-on');
+
+                    // 나머지 컨텐츠 display:none 처리
+                    cMidContents[j].style.display = 'none';   
+                }
+
+                // 버튼 관련 이벤트
+                this.parentNode.classList.add('tab-h-on');
+
+                // 버튼 클릭시 컨텐츠 전환
+                activeCont = this.getAttribute('href');
+                document.querySelector(activeCont).style.display = 'block';
+            });
+        }
+    };
 
 
 });
