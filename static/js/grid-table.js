a393db5720f0b38d2f350a6ca46229fd7e51fff4
nsphhee <parkhhee@ninesoft.biz>
2024-02-19 15:05:14 +0900

    .

--- static/js/grid-table.js
+++ static/js/grid-table.js
@@ -71,6 +71,31 @@ $(document).ready(function() {
             thead01: 'text02',
             thead02: 'text03'
         },
+        {
+            thead00: 'text01',
+            thead01: 'text02',
+            thead02: 'text03'
+        },
+        {
+            thead00: 'text01',
+            thead01: 'text02',
+            thead02: 'text03'
+        },
+        {
+            thead00: 'text01',
+            thead01: 'text02',
+            thead02: 'text03'
+        },
+        {
+            thead00: 'text01',
+            thead01: 'text02',
+            thead02: 'text03'
+        },
+        {
+            thead00: 'text01',
+            thead01: 'text02',
+            thead02: 'text03'
+        },
 
         ];
         const grid = new tui.Grid({
@@ -101,6 +126,28 @@ $(document).ready(function() {
             //  }
         });
 
+        const grid02 = new tui.Grid({
+            el: document.getElementById('grid02'),
+            data: gridData,
+            scrollX: false,
+            scrollY: false,
+            rowHeaders: ['checkbox'],
+            columns: [
+                {
+                    header: 'THEAD00',
+                    name: 'thead00'
+                  },
+                  {
+                    header: 'THEAD01',
+                    name: 'thead01',
+                    editor: 'text'
+                  },
+                  {
+                    header: 'THEAD02',
+                    name: 'thead02'
+                  }
+            ]
+        });
 });
 
 
